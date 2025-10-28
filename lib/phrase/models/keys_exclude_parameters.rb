9946165c87eb226c611b9f9e8a28ee5e32084af7
Phrase <support@phrase.com>
2025-10-28 12:08:47 +0000

    Deploying from  phrase/openapi@2e3434b6

--- lib/phrase/models/keys_exclude_parameters.rb
+++ lib/phrase/models/keys_exclude_parameters.rb
@@ -8,7 +8,7 @@ module Phrase
     # Specify a query to do broad search for keys by name (including wildcards).  The following qualifiers are also supported in the search term: - `ids:key_id,...` for queries on a comma-separated list of ids - `name:key_name` for text queries on exact key names - spaces, commas, and colons need to be escaped with double backslashes - `tags:tag_name` to filter for keys with certain tags - `translated:{true|false}` for translation status (also requires `locale_id` to be specified) - `updated_at:{>=|<=}2013-02-21T00:00:00Z` for date range queries - `unmentioned_in_upload:upload_id,...` to filter keys unmentioned within upload. When multiple upload IDs provided, matches only keys not mentioned in **all** uploads  Find more examples [here](/en/api/strings/usage-examples).     
     attr_accessor :q
 
-    # Locale used to exlcude or include keys.
+    # Exclude translations in locale
     attr_accessor :target_locale_id
 
     # Tag or comma-separated list of tags to add to the matching collection of keys
